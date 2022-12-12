beb89263c8189bc67733da2c1dd4af092a9671b9
Ajinkya Palaskar <palaskarajinkya22@gmail.com>
2022-12-12 15:42:41 +0530

    Feat: Setup basic authentication with auth0 (#4)
    Co-authored-by: Ajinkya Palaskar <ajinkya@geekyants.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Profile from "./pages/Profile/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./App.scss";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
