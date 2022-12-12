bc3e886865f84bb598aee0dbde637379f4f5ce25
Ajinkya Palaskar <palaskarajinkya22@gmail.com>
2022-12-12 11:19:49 +0530

    Feat: Add a redux slice for user (#2)
    Co-authored-by: Ajinkya Palaskar <ajinkya@geekyants.com>

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,1 +1,62 @@
-export {};
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState, AppThunk } from "../../app/store";
+
+export interface IUser {
+  name: string;
+  username: string;
+  email: string;
+  picture: string;
+  joinDate: Date;
+  following: IUser[];
+  followers: IUser[];
+  location: string;
+  bio: string;
+  isVerified: boolean;
+  //   tweets: ITweet[];
+  //   likes: ITweet[];
+  //   bookmarks: ITweet[];
+}
+
+interface IUserState {
+  currentUser: IUser | null;
+}
+
+const initialState: IUserState = {
+  currentUser: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setCurrentUser: (state, action: PayloadAction<IUser>) => {
+      state.currentUser = action.payload;
+    },
+  },
+});
+
+// get current user's profile from firebase using email
+export const fetchCurrentUser =
+  (email: string): AppThunk =>
+  async (dispatch) => {};
+
+// update user's profile details
+export const updateProfile =
+  (user: IUser): AppThunk =>
+  async (dispatch) => {};
+
+// add a user to the current user's following list
+export const follow =
+  (user: IUser): AppThunk =>
+  async (dispatch) => {};
+
+// remove a user from the current user's following list
+export const unfollow =
+  (user: IUser): AppThunk =>
+  async (dispatch) => {};
+
+export const selectCurrentUser = (state: RootState) => state.user.currentUser;
+
+export const { setCurrentUser } = userSlice.actions;
+
+export default userSlice.reducer;
