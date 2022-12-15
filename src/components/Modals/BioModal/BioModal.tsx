1739c5bf8dd5368be76d39fcf7195efecb72d6e6
Ajinkya Palaskar <ajinkya@geekyants.com>
2022-12-15 17:59:31 +0530

    Feat: Modal for getting user bio and location during onboarding

--- src/components/Modals/BioModal/BioModal.tsx
+++ src/components/Modals/BioModal/BioModal.tsx
@@ -6,16 +6,20 @@ import Form from "react-bootstrap/Form";
 type Props = {
   show: boolean;
   onHide: (e: React.FormEvent<HTMLFormElement>) => void;
-  username: string;
-  setUsername: React.Dispatch<React.SetStateAction<string>>;
+  bio: string;
+  setBio: React.Dispatch<React.SetStateAction<string>>;
+  location: string;
+  setLocation: React.Dispatch<React.SetStateAction<string>>;
   handleBack: () => void;
 };
 
-const UsernameModal = ({
+const BioModal = ({
   show,
   onHide,
-  username,
-  setUsername,
+  bio,
+  setBio,
+  location,
+  setLocation,
   handleBack,
 }: Props) => {
   return (
@@ -54,29 +58,42 @@ const UsernameModal = ({
             </svg>
           </Button>
 
-          <h4 className="m-0">Step 2 of 4</h4>
+          <h4 className="m-0">Step 3 of 4</h4>
         </Modal.Title>
       </Modal.Header>
 
       <Modal.Body className="border-none mt-3 px-5">
-        <h2 className="pb-1">Pick your username</h2>
+        <h2 className="pb-1">Tell us about yourself</h2>
 
         <Form onSubmit={onHide}>
+          {/* bio */}
           <Form.Control
             type="text"
             className="form-control p-3 mt-4 mb-2 border-2"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value.toLowerCase())}
+            placeholder="Your Twitter Bio"
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
           />
 
           <Form.Text className="text-muted">
-            This will be your unique handle on the site.
+            This will be shown on your profile.
+          </Form.Text>
+
+          {/* location */}
+          <Form.Control
+            type="text"
+            className="form-control p-3 mt-4 mb-2 border-2"
+            placeholder="Where are you from?"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+
+          <Form.Text className="text-muted">
+            Enter your city or country.
           </Form.Text>
 
           <Button
-            variant={!username ? "secondary" : "primary"}
-            disabled={!username}
+            variant={"primary"}
             type="submit"
             className="rounded-pill w-100 py-2 fw-bold mt-5"
           >
@@ -88,4 +105,4 @@ const UsernameModal = ({
   );
 };
 
-export default UsernameModal;
+export default BioModal;
