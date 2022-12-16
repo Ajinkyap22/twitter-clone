9114405423a75ff0f46db36405e3ecbd6acf9f84
Ajinkya Palaskar <palaskarajinkya22@gmail.com>
2022-12-16 13:16:14 +0530

    Feat: Add sticky headers for feed content & suggestion sections (#13)
    Co-authored-by: Ajinkya Palaskar <ajinkya@geekyants.com>

---
+++ src/components/Suggestions/Suggestions.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
+const Suggestions = () => {
+  return (
+    <>
+      {/* search bar */}
+      <SearchBar />
+    </>
+  );
+};
+
+export default Suggestions;
