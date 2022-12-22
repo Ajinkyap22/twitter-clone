0aaa421b54a732bc735882f46a828b973b365f02
Damini Pandey <daminip@geekyants.com>
2022-12-22 12:36:27 +0530

    Feat: Toggle between the bookmark feature

--- src/components/TweetCard/TweetCard.tsx
+++ src/components/TweetCard/TweetCard.tsx
@@ -174,7 +174,7 @@ const TweetCard = ({ tweet }: TweetCardProps): JSX.Element => {
           onClick={handleTweetBookmark}
         >
           {" "}
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -187,8 +187,44 @@ const TweetCard = ({ tweet }: TweetCardProps): JSX.Element => {
               strokeLinejoin="round"
               d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
             />
-          </svg>
-          Bookmark Tweet
+          </svg> */}
+          {isBookmarked ? (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-4 h-4 me-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 3l1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 011.743-1.342 48.507 48.507 0 0111.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664L19.5 19.5"
+                />
+              </svg>
+              Remove Bookmark
+            </>
+          ) : (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-4 h-4 me-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                />
+              </svg>
+              Add Bookmark
+            </>
+          )}
         </Button>
       </Popover.Body>
     </Popover>
